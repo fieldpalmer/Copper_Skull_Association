e9152e25f47da8bf153ee3cdfb28234993fe186e
aimontano <aimorales.montano@gmail.com>
2018-11-08 23:50:45 -0500

    handle quote me button to load template

--- public/js/templates.js
+++ public/js/templates.js
@@ -6,6 +6,18 @@ $(document).ready(function(){
 const loadEventHandlers = () => {
 	// handle get a quote button click
 	$(document).on("click", "#btnQuote", e => {
-		$('#main').load('../public/templates/quote.html');
+		// when user clicks on get quote button, load quote template
+		$('#main').load('templates/quote.html');
+
+		// remove button
+		$("#btnQuote").remove();
+	});
+
+	$(document).on("click", "#btnQuoteMe", e => {
+		// before loading the estimate template validate user input
+		// send information to database
+		// calculate cost
+		// insert information to template
+		$('#main').load('templates/estimate.html');
 	})
 }
