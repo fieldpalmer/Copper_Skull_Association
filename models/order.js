185efb2db1052a7f41659983a8c0198444019c1d
Ethan <ethan.holston@gmail.com>
2018-11-10 18:13:05 -0500

    initial commit

---
+++ models/order.js
@@ -0,0 +1,36 @@
+module.exports = function(sequelize, DataTypes) {
+
+  const Order = sequelize.define('Order', {
+
+    jobComplete: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'Pending'
+    },
+
+    jobDescription: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+
+    laborCost: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    }
+
+  });
+
+  Order.associate = function(models) {
+
+    Order.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+      
+    });
+
+    //belongs to product
+  };
+
+  return Order;
+};
