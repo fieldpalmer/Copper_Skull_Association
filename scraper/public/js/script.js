712fd11c58b0115f351dd15db351819e4d03d66d
aimontano <aimorales.montano@gmail.com>
2018-11-10 16:46:32 -0500

    Able to load the years

--- scraper/public/js/script.js
+++ scraper/public/js/script.js
@@ -7,13 +7,44 @@ const setEventHandler = () => {
 	$(document).on('click', 'a', function(){
 		let makeLink = $(this).data('make');
 		let makeName = $(this).text();
+		displayYear({makeName: makeName, makeLink: makeLink});
+	});
+
+	$(document).on('click', 'a', function(){
+		let carYear = $(this).data('year');
+		// displayModel(carYear);
 	})
+}
+
+const displayYear = car => {
+	$.ajax('/api/year/', {
+		method: "POST",
+		data: {
+			makeName: car.makeName,
+			url: car.makeLink
+		}
+	}).then(car => {
+		let years = car.year; // year array
+		$('#app').load('/templates/year.html', function() {
+			let row = $('<div>').addClass('row');
+			years.forEach(year => {
+				let div = $('<div>').addClass('col-md-2');
+				let y = $('<a>').attr('data-year', year);
+
+				y.text(year);
+				y.attr('href', '#');
+
+				div.append(y);
+				row.append(div);
+			});
+			$('#year').append(row);
+		});
+	});
 }
 
 const displayMake = () => {
 	$.get('/api/make').then(makes => {
 		$("#app").load('/templates/make.html', function() {
-			console.log("Loaded");
 			let row = $('<div>').addClass('row');
 			makes.forEach(car => {
 				let div = $("<div>").addClass('col-md-2 ');
