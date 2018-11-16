3aedbbe2189ddee2479100de4c71a723fa4adcba
Ethan <ethan.holston@gmail.com>
2018-11-15 20:05:22 -0500

    added quote model

---
+++ models/quote.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  const Quote = sequelize.define('Review', {
+    carMake: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    carModel: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    carEngine: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quoteAmt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      } 
+    },
+    oilType: {
+      type: DataTypes.STRING
+    },
+    oilAmount: {
+      type: DataTypes.STRING
+    }
+  });
+  return Quote;
+};
