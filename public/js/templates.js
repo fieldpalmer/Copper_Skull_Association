30c10af5f07c02b05cc9573229343f3a0eb8ae40
Ethan <ethan.holston@gmail.com>
2018-11-16 22:48:48 -0500

    fixed register issues, login working

--- public/js/templates.js
+++ public/js/templates.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
    $(document).on("click", "#bottom-nav-contact", () => {loadContact()});
    $(document).on("click", "#bottom-nav-work", () => {loadAuth()});
 
-	displayQuoteTemplate('#calculator');
+	//displayQuoteTemplate('#calculator');  //getting displayQuoteTemplate is undefined error
 
 	$(document).on("click", "#btnQuoteMe", function() {
 		// before loading the estimate template validate user input
@@ -30,64 +30,42 @@ $(document).ready(function(){
 		let phone = $('#txtPhone').val().trim();
 		let email = $('#txtEmail').val().trim();
 
-		if(!car.make || !car.year || !car.model) {
-			showErrMessage("You must select your car make/year/model");
-		}
-		// check if user has selected a service
-		else if (!car.services) {
-			showErrMessage("You must select a service")
-		}
-		// check if user has entered a valid address
-		else if(breakAddress(address) === false) {
-			showErrMessage("You must enter your adress");
-		}
-		// if everything passes...
-		else {
-			let customer = {
-				firstName: firstName,
-				lastName: lastName,
-				phone: phone,
-				email: email,
-				address: breakAddress(address)
+		console.log(window.car);
+		$.post("/api/quote", {
+			info: car.info.quartsCapacity
+		}).then(function(response) {
+			console.log(response);
+			if(!car.make || !car.year || !car.model) {
+				showErrMessage("You must select your car make/year/model");
 			}
-
-			car.customer = customer;
-
-			// get a quote from the server by sending quarts capacity
-			$.post("/api/quote", {
-				info: car.info.quartsCapacity
-			}).then(function(response) {
-				console.log(response);
-				if(!car.make || !car.year || !car.model) {
-					showErrMessage("You must select your car make/year/model");
+			// check if user has selected a service
+			else if (!car.services) {
+				showErrMessage("You must select a service")
+			}
+			// check if user has entered a valid address
+			else if(breakAddress(address) === false) {
+				showErrMessage("You must enter your adress");
+			}
+			// if everything passes...
+			else {
+				car.address = breakAddress(address);
+					// load  estimate
+				car.customer = {
+					firstName: firstName,
+					lastName: lastName,
+					phone: phone,
+					email: email,
+					address: breakAddress(address)
 				}
-				// check if user has selected a service
-				else if (!car.services) {
-					showErrMessage("You must select a service")
-				}
-				// check if user has entered a valid address
-				else if(breakAddress(address) === false) {
-					showErrMessage("You must enter your adress");
-				}
-				// if everything passes...
-				else {
-					car.address = breakAddress(address);
-						// load  estimate
-					car.customer = {
-						firstName: firstName,
-						lastName: lastName,
-						phone: phone,
-						email: email,
-						address: breakAddress(address)
-					}
-					$("#main").load('templates/estimate.html', function() {
-						$('#carInfo').text(`${car.year} ${car.make} ${car.model}`);
-						$('#oilType').text(car.info.oilType);
-						$('#oilCapacity').text(car.info.quartsCapacity);
-						$('#totalCost').text(response);
-					});
-				}
-			});
+				$("#main").load('templates/estimate.html', function() {
+					$('#carInfo').text(`${car.year} ${car.make} ${car.model}`);
+					$('#oilType').text(car.info.oilType);
+					$('#oilCapacity').text(car.info.quartsCapacity);
+					$('#totalCost').text(response);
+				});
+			}
+		});
+	});
 
 	$(document).on("click", "#btnBook", function() {
 		// when user clicks schedule appointment load appointment template
@@ -112,54 +90,22 @@ $(document).ready(function(){
 			showErrMessage("Please select a day and a time");
 		} else {
 			car.appointment = appointment;
-
-			if(!isAvailable(date, time)) {
-				showErrMessage("You must select an upcoming date and a time between 9 - 5");
-			} else {
-				console.log("Booked!!")
-				// send information to server for work order
-				/*
-				send the following information to server:
-					- user id
-					- services
-					- tech selected
-					- labor cost (should send the qts again)
-				*/
-				$.post("/api/orders", {
-					jobDescription: car.services.join(', '),
-					laborCost: car.laborCost,
-					customer_id: car.uid
-				}).then(data => {
-					let confirmNum = data.id;
-					let job = data.jobDescription;
-					let cost = data.laborCost;
-					let status = data.jobComplete;
-					appointment = date + ' at ' + time
-
-					$('#calculator').load('templates/workorder.html', function() {
-						$('#confirmation').text(confirmNum);
-						$('#description').text(job);
-						$('#cost').text(`$${cost}`);
-						$('#tech').text('Not Chosen');
-						$('#appointment').text(appointment);
-					})
-				});
-				// when response
-				// $("#main").load("templates/workorder.html");
-			}
+			// send information to server
+			// when response
+			// $("#main").load("templates/workorder.html");
 		}
 	})
 
-	$(document).on("click", "#registerBtn", function() {
-
+	$(document).on("click", "#registerBtn", function(event) {
+		event.preventDefault();
 		let newUser = {
 			fName: $("#fName").val().trim(),
 			lName: $("#lName").val().trim(),
-			email: $("#email").val().trim(),
+			email: $("#emailReg").val().trim(),
 			phone: $("#phone").val().trim(),
-			areaCode: $("#areaCode").val().trim(),
-			password: $("#password").val().trim(),
-			picture: $("#picture").src(),
+			// areaCode: $("#areaCode").val().trim(),
+			password: $("#passwordReg").val().trim(),
+			// picture: $("#picture").src()
 		}
 
 		$.post("/api/register", newUser).then(function(response) {
@@ -168,12 +114,13 @@ $(document).ready(function(){
 
 		$("#fName").val("");
 		$("#lName").val("");
-		$("#email").val("");
+		$("#emailReg").val("");
 		$("#phone").val("");
+		$("#txtAddress").val("");
 		$("#areaCode").val("");
-		$("#password").val("");
+		$("#passwordReg").val("");
 		$("#pwConfirm").val("");
-		$("#picture").src("");
+		// $("#picture").src("");
 		 
 	});
 
