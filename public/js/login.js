fec1c4662286b1718764710617ae3d671203c0c4
Ethan <ethan.holston@gmail.com>
2018-11-17 17:13:03 -0500

    login working

--- public/js/login.js
+++ public/js/login.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", function(event) {
+    event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
@@ -28,6 +29,7 @@ $(document).ready(function() {
       password: password
     }).then(function(data) {
       window.location.replace(data);
+      // If there's an error, log the error
     });
   }
 
