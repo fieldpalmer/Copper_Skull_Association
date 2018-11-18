5ae6d92569803789a4aaf531b107230ec942917d
Ethan <ethan.holston@gmail.com>
2018-11-18 13:04:46 -0500

    fixed order flow

---
+++ routes - Copy/stripe-api-routes.js
@@ -0,0 +1,34 @@
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+
+const stripe = require("stripe")(keySecret);
+const axios = require("axios");
+
+
+module.exports = function(app) {
+  app.post("/charge", function(req, res) {
+      console.log(req.body.quoteId);  
+      axios.get("http://localhost:8088/api/quote/" + req.body.quoteId).then(function(response){
+      console.log(response.data.quoteAmt);
+      var amount = response.data.quoteAmt * 100;
+      stripe.customers.create({
+        email: req.body.email,
+        card: req.body.id
+      })
+      .then(customer =>
+        stripe.charges.create({
+          amount: amount,
+          description: "Sample Charge",
+          currency: "usd",
+          customer: customer.id
+        }))
+      .then(charge => res.send(charge))
+      .catch(err => {
+        console.log("Error:", err);
+        res.status(500).send({error: "Purchase Failed"});
+      });
+    });
+  });
+
+    
+}
