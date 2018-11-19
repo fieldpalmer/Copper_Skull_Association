b14a6189c48508d742aeaa570f18f9a9b8eabbba
aimontano <aimorales.montano@gmail.com>
2018-11-19 15:52:35 -0500

    revert to unbroken code

--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -89,9 +89,7 @@ $(document).ready(function() {
       review.rating = $("#review-rating").val();
       review.reviewText = $("#review-text").val();
       $.post("/api/review", review).then(function(){
-        $.post("/api/orders/review/" + review.order_id).then(function(){
         location.reload();
-        });
       });
     });
   });
